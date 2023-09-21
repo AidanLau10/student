5ddcbd2dd939b036674171ed70570d05c0a5d613
AidanLau10 <aidanlau10@gmail.com>
2023-09-20 19:18:53 -0700

    added team test

--- homework-list.py
+++ homework-list.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 
 app = Flask(__name__)
 
-# Replace with your Canvas API endpoint and access token
+
 base_url = 'https://poway.instructure.com/api/v1/'
 headers = {
     'Authorization': 'Bearer 2573~mbqfLD2yTcQr8StVbsAHhSC695LjGUSw9SCeRd0x8qysh1uJ5Jvo77uu9IRez34d'
